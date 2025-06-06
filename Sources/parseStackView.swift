5f560b670723164d3b24e1f86164d9ed24460fe3
Blazej Sleboda <5544365+Adobels@users.noreply.github.com>
2025-06-06 18:38:26 +0200

    Print StackView atrributes to code

--- Sources/parseStackView.swift
+++ Sources/parseStackView.swift
@@ -7,14 +7,33 @@
 
 import StoryboardDecoder
 
+struct UIStackView {
+    let stackView: StackView
+    init(_ stackView: StackView) {
+        self.stackView = stackView
+    }
+    var axis: String { stackView.axis }
+    var alignment: String? { stackView.alignment }
+    var distribution: String? { stackView.distribution }
+    var isBaselineRelativeArrangement: Bool? { stackView.baselineRelativeArrangement }
+    var spacing: Int? { stackView.spacing }
+}
+
 func parseStackViewAttributes(_ stackView: StackView) -> [String] {
     var attributes: [String] = []
+    let stackView = UIStackView(stackView)
     attributes.append("axis == .\(stackView.axis)")
     if let alignment = stackView.alignment {
         attributes.append("alignment = .\(alignment)")
     }
     if let distribution = stackView.distribution {
         attributes.append("distribution = .\(distribution)")
+    }
+    if let spacing = stackView.spacing {
+        attributes.append("spacing = \(spacing)")
+    }
+    if let isBaselineRelativeArrangement = stackView.isBaselineRelativeArrangement {
+        attributes.append("isBaselineRelativeArrangement = \(isBaselineRelativeArrangement)")
     }
     return attributes
 }
