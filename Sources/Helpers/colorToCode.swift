6c517d26e624ea407b88d998d11e91bc91e36bc3
Blazej Sleboda <5544365+Adobels@users.noreply.github.com>
2025-07-21 21:36:53 +0200

    Fix colors are missing closing bracket

--- Sources/Helpers/colorToCode.swift
+++ Sources/Helpers/colorToCode.swift
@@ -16,12 +16,12 @@ func colorToCode(_ color: Color) -> String {
         //UIColor(cgColor: .init(red: 0.333, green: 0.333, blue: 0.333, alpha: 1))
         return ".init(cgColor: .init(red: \(calibratedRGB.red), green: \(calibratedRGB.green), blue: \(calibratedRGB.blue), alpha: \(calibratedRGB.alpha)))"
     case .sRGB(let sRGB):
-        return ".init(red: \(sRGB.red), green: \(sRGB.green), blue: \(sRGB.blue), alpha: \(sRGB.alpha)"
+        return ".init(red: \(sRGB.red), green: \(sRGB.green), blue: \(sRGB.blue), alpha: \(sRGB.alpha))"
     case .gamma22Gray(let gamma22Gray):
         return ".init(cgColor: .init(genericGrayGamma2_2Gray: \(gamma22Gray.white), alpha: \(gamma22Gray.alpha)))"
     case .name(let named):
        return  ".init(named: \(named))"
     case .systemColor(let named):
-        return ".init(named: \(named)"
+        return ".init(named: \(named))"
     }
 }
