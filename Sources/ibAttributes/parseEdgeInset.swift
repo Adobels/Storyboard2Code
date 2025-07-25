8e2986425d00c1f0c7e26e8cd612d2f5668b0f73
Blazej Sleboda <5544365+Adobels@users.noreply.github.com>
2025-07-25 22:44:19 +0200

    return .zero instead full init method in case when it is possible

--- Sources/ibAttributes/parseEdgeInset.swift
+++ Sources/ibAttributes/parseEdgeInset.swift
@@ -9,5 +9,9 @@ import StoryboardDecoder
 
 func parseEdgeInset(_ edgeInset: EdgeInset?) -> String? {
     guard let edgeInset else { return nil }
-    return ".init(top: \(edgeInset.top ?? 0), left: \(edgeInset.left ?? 0), bottom: \(edgeInset.bottom ?? 0), right: \(edgeInset.right ?? 0)"
+    return if edgeInset.top == 0, edgeInset.left == 0, edgeInset.right == 0, edgeInset.bottom == 0 {
+        ".zero"
+    } else {
+        ".init(top: \(edgeInset.top ?? 0), left: \(edgeInset.left ?? 0), bottom: \(edgeInset.bottom ?? 0), right: \(edgeInset.right ?? 0)"
+    }
 }
--- Sources/ibAttributes/parseInset.swift
+++ Sources/ibAttributes/parseInset.swift
@@ -9,7 +9,11 @@ import StoryboardDecoder
 
 func parseInset(_ inset: Inset?) -> String? {
     guard let inset else { return nil }
-    return ".init(top: \(inset.minY ?? 0), left: \(inset.minX ?? 0), bottom: \(inset.maxY ?? 0), right: \(inset.maxX ?? 0))"
+    return if inset.minY == 0, inset.minX == 0, inset.maxY == 0, inset.maxX == 0 {
+        ".zero"
+    } else {
+        ".init(top: \(inset.minY ?? 0), left: \(inset.minX ?? 0), bottom: \(inset.maxY ?? 0), right: \(inset.maxX ?? 0))"
+    }
 }
 
 // In InterfaceBuilder: Left -> 1, Top -> 2, Bottom -> 3, Right -> 4
